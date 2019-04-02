58103cd4f1536ea224e0fc88debc80b538451896
Thomas Harold <tgharold@gmail.com>
2019-04-02 09:52:09 -0400

    creating an initial migration

---
+++ test/FluentMigratorTests.Tests/Migrations/2019040201_Initial.cs
@@ -0,0 +1,13 @@
+using FluentMigrator;
+
+namespace FluentMigratorTests.Tests.Migrations
+{
+    [Migration(2019040201)]
+    public class Initial : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
