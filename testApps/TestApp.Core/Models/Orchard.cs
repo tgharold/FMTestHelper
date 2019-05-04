4a415b72256863b30fc86fd68fae93a239d85138
Thomas Harold <tgharold@gmail.com>
2019-05-04 11:22:34 -0400

    remove EF context/data models

--- testApps/TestApp.Core/Models/Orchard.cs
+++
@@ -1,9 +0,0 @@
-namespace TestApp.Core.Models
-{
-    public class Orchard
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
